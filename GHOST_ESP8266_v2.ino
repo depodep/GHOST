3d2f7585a54014ecf52904325348d4f73a7d0d41
Russell Jheiss Sarceda <russelljheiss.sarceda@isu.edu.ph>
2026-05-20 14:51:30 +0800

    sasa

--- GHOST_ESP8266_v2.ino
+++ GHOST_ESP8266_v2.ino
@@ -45,7 +45,7 @@ const char* WIFI_PASSWORD = "Incubator2026";
 // ─────────────────────────────────────────────
 
 // const char* SERVER_IP     = "192.168.70.46";  
-const char* SERVER_IP     = "192.168.70.46";  
+const char* SERVER_IP     = " 192.168.70.46";  
 const int   SERVER_PORT   = 80;               
 const char* SERVER_BASE_PATH = "/GHOST";       
 const int   INCUBATOR_ID  = 1;                 
